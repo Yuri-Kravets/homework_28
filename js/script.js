17f7ca76b8b4b2bc2a6cec59106c51d54a42fc6f
Yuri-Kravets <kravets.yuri.v@gmail.com>
2021-12-21 14:22:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
-"use strict"
+"use strict";
 
-let calculator = {
-    read(a,b) {
-        if(typeof a === 'number' && !isNaN(a)) return 'error';
-        if(typeof b === 'number' && !isNaN(b)) return 'error';
+(function () {
+    let calculator = {
+        read(a,b) {
+            if(typeof a === 'number' && isNaN(a)) return 'error';
+            if(typeof b === 'number' && isNaN(b)) return 'error';
+            
+            this.a = a;
+            this.b = b;
+        },
         
-        this.a = a;
-        this.b = b;
-    },
-    
-    sum() {
-        if(!this.a && !this.b) return 'error';
-        return this.a + this.b
+        sum() {
+            if(!this.a && !this.b) return 'error';
+            return this.a + this.b
+            
+          },
         
-      },
-    
-    mul() {
-        if(!this.a && !this.b) return 'error';
-        return this.a * this.b
-      }
-    };
-    
-calculator.read(6,12);
-alert(calculator.sum());
-alert(calculator.mul());
+        mul() {
+            if(!this.a && !this.b) return 'error';
+            return this.a * this.b
+          }
+        };
+        
+    calculator.read(9,12);
+    alert(calculator.sum());
+    alert(calculator.mul());
+})();
+
